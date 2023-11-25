2f804cfd13ab6157ccf71a217656f2d651991513
urvishshiroya2023 <urvish.shiroya@sarvadhi.com>
2023-11-25 13:49:13 +0530

    taskinfo added

--- src/Components/Constants.js
+++ src/Components/Constants.js
@@ -1,1 +1,16 @@
 export const BASE_URL = "https://crmapi.sarvadhi.work/api/v1/"
+
+export const getPriorityColor = (priority) => {
+    switch (priority) {
+        case 'low':
+            return 'bg-green-100 text-green-600';
+        case 'medium':
+            return 'bg-orange-100 text-orange-600';
+        case 'high':
+            return 'bg-red-100 text-red-600';
+        case 'none':
+            return 'bg-gray-100 text-gray-600';
+        default:
+            return '';
+    }
+};
--- src/Components/TaskInfo.js
+++ src/Components/TaskInfo.js
@@ -1,26 +1,75 @@
-import React from 'react';
+import axios from 'axios';
+import React, { useEffect, useState } from 'react';
 import { Link, useParams } from 'react-router-dom';
+import { getPriorityColor } from './Constants';
 import HomePageHeader from './HomePageHeader';
 
 const TaskInfo = () => {
-
     const { id } = useParams();
+    const [taskData, setTaskData] = useState(null);
 
-    console.log(id);
+    const priorityColor = getPriorityColor(taskData?.priority);
+
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                const authToken = localStorage.getItem('authToken');
+                const headers = {
+                    'Content-Type': 'application/json',
+                    Authorization: `Bearer ${authToken}`,
+                };
+
+                const response = await axios.get(`https://crmapi.sarvadhi.work/api/v1/crm/tasks/${id}`, { headers });
+
+                setTaskData(response?.data?.data);
+            } catch (error) {
+                console.error('Error fetching task data:', error);
+                // Handle error as needed
+            }
+        };
+
+        fetchData();
+    }, [id]);
+
     return (
-        <div>
+        <div className='bg-[#e5e7eb] bg-opacity-50 h-full min-h-screen'>
             <div>
                 <HomePageHeader />
             </div>
             <div className='flex mt-5 items-center justify-center'>
                 <div className='container'>
-                    <Link to={"/taskdetail"}>
-                        <h6> &larr; Task Details</h6>
-                    </Link>
-                </div>
-            </div>
-        </div>
-    )
-}
+                    <div>
 
-export default TaskInfo
+
+                        <Link to="/taskdetail">
+                            <h3 className='text-2xl'> &larr; Task Details</h3>
+                        </Link>
+                    </div>
+                    <div className='mt-5 bg-white p-5 border rounded-md'>
+                        {taskData ? (
+                            <div className='grid grid-cols-5'>
+                                <div className={`text-[#6B7280] text-sm font-semibold `}>Priority : <span className='font-light'> {taskData?.priority}</span></div>
+                                <div className='text-[#6B7280] text-sm font-semibold'>Status : <span className='font-light'> {taskData?.taskStatus?.statusName}</span></div>
+                                <div className='text-[#6B7280] text-sm font-semibold'>Due Date <span className='font-light'> : {taskData?.dueDate}</span></div>
+                                <div className='text-[#6B7280] text-sm font-semibold'>Title : <span className='font-light'> {taskData?.title}</span></div>
+                                <div className='text-[#6B7280] text-sm font-semibold'>Module : <span className='font-light'> {taskData?.module}</span></div>
+                                <div className='text-[#6B7280] text-sm font-semibold'>Type : <span className='font-light'> {taskData?.type}</span></div>
+
+                            </div>
+                        ) : (
+                            <p>Loading task details...</p>
+                        )}
+                    </div>
+
+                </div>
+
+
+            </div>
+
+
+
+        </div>
+    );
+};
+
+export default TaskInfo;
