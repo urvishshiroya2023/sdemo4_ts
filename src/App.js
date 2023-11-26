ad876e98dc5e3f87e237c29f428c9a7134ff93e4
Urvish <urvishshiroya262000@gmail.com>
2023-11-26 22:30:14 +0530

    protected routes added

--- src/App.js
+++ src/App.js
@@ -1,20 +1,25 @@
-import { BrowserRouter, Route, RouterProvider, Routes, createBrowserRouter } from 'react-router-dom';
-import { ToastContainer } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
-import './App.css';
-import Body from './Components/Body';
-import Contacts from './Components/Contacts';
-import Deal from './Components/Deal';
-import HomePage from './Components/HomePage';
-import Leads from './Components/Leads';
-import OnBoardComponent from './Components/OnBoardComponent';
-import ProtectedRoutes from './Components/ProtectedRoutes';
-import SignInComponent from './Components/SignInComponent';
+import {
+  BrowserRouter,
+  Route,
+  RouterProvider,
+  Routes,
+  createBrowserRouter,
+} from "react-router-dom";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import "./App.css";
+import Body from "./Components/Body";
+import Contacts from "./Components/Contacts";
+import Deal from "./Components/Deal";
+import HomePage from "./Components/HomePage";
+import Leads from "./Components/Leads";
+import OnBoardComponent from "./Components/OnBoardComponent";
+import ProtectedRoutes from "./Components/ProtectedRoutes";
+import SignInComponent from "./Components/SignInComponent";
 import TaskDetail from "./Components/TaskDetail";
-import TaskForm from './Components/TaskForm';
-import TaskInfo from './Components/TaskInfo';
+import TaskForm from "./Components/TaskForm";
+import TaskInfo from "./Components/TaskInfo";
 import "./index.css";
-
 
 const appRouter = createBrowserRouter([
   {
@@ -23,62 +28,60 @@ const appRouter = createBrowserRouter([
     children: [
       {
         path: "/signin",
-        element: <SignInComponent />
+        element: <SignInComponent />,
       },
       {
         path: "/onboarduser",
-        element: <OnBoardComponent />
+        element: <OnBoardComponent />,
       },
       {
         path: "/homepage",
-        element: <HomePage />
+        element: <HomePage />,
       },
       {
         path: "/contacts",
-        element: <Contacts />
+        element: <Contacts />,
       },
       {
         path: "/leads",
-        element: <Leads />
+        element: <Leads />,
       },
       {
         path: "/deal",
-        element: <Deal />
+        element: <Deal />,
       },
       {
         path: "/taskdetail",
-        element: <TaskDetail />
+        element: <TaskDetail />,
       },
       {
         path: "/addtask",
-        element: <TaskForm />
+        element: <TaskForm />,
       },
       {
         path: "/taskdetails/:id",
-        element: <TaskInfo />
-      }
-    ]
-  }
-])
+        element: <TaskInfo />,
+      },
+    ],
+  },
+]);
 
 function App() {
-
-
-
   return (
     <div className="">
-
       <BrowserRouter>
         <Routes>
-          <Route path='/' element={<ProtectedRoutes />}>
-            <Route path='/' element={<HomePage />} />
+          <Route path="/" element={<ProtectedRoutes />}>
+            <Route path="/homepage" element={<HomePage />} />
+            <Route path="/taskdetail" element={<TaskDetail />} />
+            <Route path="/deal" element={<Deal />} />
+            <Route path="/leads" element={<Leads />} />
+            <Route path="/contacts" element={<Contacts />} />
           </Route>
         </Routes>
       </BrowserRouter>
       <RouterProvider router={appRouter} />
       <ToastContainer />
-
-
     </div>
   );
 }
--- src/Components/ProtectedRoutes.js
+++ src/Components/ProtectedRoutes.js
@@ -1,12 +1,24 @@
-import React from 'react';
-import { Navigate } from 'react-router-dom';
-import Body from './Body';
+// import React from 'react';
+// import { Navigate } from 'react-router-dom';
+// import Body from './Body';
+
+// const ProtectedRoutes = () => {
+//     const auth = localStorage.getItem("authToken");
+//     return (
+//         auth ? <Body /> : <Navigate to={"/signin"} />
+//     )
+// }
+
+// export default ProtectedRoutes
+
+import React from "react";
+import { Navigate } from "react-router-dom";
+import { useAuth } from "./AuthContext";
+import Body from "./Body";
 
 const ProtectedRoutes = () => {
-    const auth = localStorage.getItem("authToken");
-    return (
-        auth ? <Body /> : <Navigate to={"/signin"} />
-    )
-}
+  const { token } = useAuth();
+  return token ? <Body /> : <Navigate to="/signin" />;
+};
 
-export default ProtectedRoutes
+export default ProtectedRoutes;
